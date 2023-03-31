c1e907734cdf7c5ed2ff0a7ace4a69e549204781
Wasswa Enock Male <enockmale97@gmail.com>
2023-03-31 13:37:11 +0300

    posts to the database now

--- phonebook/index.js
+++ phonebook/index.js
@@ -25,31 +25,31 @@ morgan.token('body',(req, res)=>{
 
 app.use(morgan('body'));
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Yes, it works</h1>')
+    response.send('<h1>Yes, it works</h1>');
 });
 
 
@@ -115,28 +115,36 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons/', (request, response) => {
     const body = request.body;
     // console.log(body)
-    if(!body.name || !body.number){
+    if(body.name && body.number){
+        const phonebook = new PhoneBook({
+            id: generateId(),
+            name: body.name,
+            number: body.number,
+        });
+        
+        phonebook.save().then(savedNumber => {
+            response.json(savedNumber);
+        });
+    }else{
         return response.status(400).json({
             error: `Either the name or the number is missing.`,
         });
-    }else{
-
-        const nam = persons.find(element => element.name === body.name);
-        if(nam){
-            return response.status(400).json({
-                error: 'Name already exists',
-            });
-        }else{
-            const person = {
-                id: generateId(),
-                name: body.name,
-                number: body.number
-            }
-    
-            persons.concat(person);
-            response.json(person);
-        }
     }
+        // const nam = persons.find(element => element.name === body.name);
+        // if(nam){
+        //     return response.status(400).json({
+        //         error: 'Name already exists',
+        //     });
+        // }else{
+        //     const person = {
+        //         id: generateId(),
+        //         name: body.name,
+        //         number: body.number
+        //     }
+    
+        //     persons.concat(person);
+        //     response.json(person);
+        // }
 });
 
 const PORT = process.env.PORT;
