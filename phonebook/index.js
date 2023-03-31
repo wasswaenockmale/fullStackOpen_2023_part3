1cfe3ea1f42c7ad5eba8e87dbd2fe8ed3490a8ad
Wasswa Enock Male <enockmale97@gmail.com>
2023-03-31 13:19:45 +0300

    commented the require for mongoose

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const PhoneBook = require('./models/phonenumber');
 require('dotenv').config();
 
